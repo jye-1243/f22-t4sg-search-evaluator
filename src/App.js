cf8b957f97f2765fe7acd917fc76bbc41a56cc16
Parita Shah <PARITASHAH@COLLEGE.HARVARD.EDU>
2022-10-06 18:31:58 -0400

    rough sign in page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 function App() {
+  const [ profile, setProfile ] = useState([]);
+  const clientId = '1044211576984-ieq8c2m6h75hqb24dvgrtol8krfdvci4.apps.googleusercontent.com';
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+      clientId: clientId,
+      scope: '',
+      });
+    };
+  gapi.load('client:auth2', initClient);
+  });
+  const onSuccess = (res) => {
+    console.log('success:', res);
+  };
+  const onFailure = (err) => {
+    console.log('failed:', err);
+  };
+  const logOut = () => {
+    setProfile(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h2>Cambrdige Search Evaluator</h2>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText="Sign in with Google"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
+            />
     </div>
   );
 }
