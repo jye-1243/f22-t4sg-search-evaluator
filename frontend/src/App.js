8e3bd357724cac5cc5bf795951b5ad0c1ed6030f
JUSTIN YE <justinye@college.harvard.edu>
2022-10-11 12:07:39 -0400

    Reorganize repo and move query UI into component

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import QueryBlock from './components/QueryBlock'
+import React from 'react';
+import {useState} from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <QueryBlock />
+      <QueryBlock />
+
+    </div>
+
+  )
+}
+export default App;
